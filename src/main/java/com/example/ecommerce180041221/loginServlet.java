b0d12170801e7ef30bcd843e8e5261c473ec6432
samiul islam <78860039+samiulabir201@users.noreply.github.com>
2022-04-22 23:36:38 +0600

    Add files via upload

---
+++ src/main/java/com/example/ecommerce180041221/loginServlet.java
@@ -0,0 +1,20 @@
+package com.example.ecommerce180041221;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+//This is the login servlet
+@WebServlet(name = "com.example.ecommerce180041221.loginServlet", value = "/com.example.ecommerce180041221.loginServlet")
+public class loginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
